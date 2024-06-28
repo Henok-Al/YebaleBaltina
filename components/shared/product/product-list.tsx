5c13390df73968757bb09e0dc728f856e6a1562a
Henok-Al <henok.gebresenbet@gmail.com>
2024-06-28 23:23:06 +0300

    fetch data from database

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+
    import ProductCard from './product-card'
-   const ProductList = ({ data }: { data: any }) => {
+   import { Product } from '@/types'
+
+   const ProductList = ({
+     title,
+     data,
+   }: {
+     title: string
+     data: Product[]
+   }) => {
      return (
-       <>
+       <section className="my-10">
+         <h2 className="h2-bold mb-4">{title}</h2>
          {data.length > 0 ? (
            <div>
              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-               {data.map((product: any) => (
+               {data.map((product: Product) => (
                  <ProductCard key={product.slug} product={product} />
                ))}
              </div>
@@ -16,7 +26,8 @@ import React from 'react'
              <p>No product found</p>
            </div>
          )}
-       </>
+       </section>
      )
    }
+
    export default ProductList
