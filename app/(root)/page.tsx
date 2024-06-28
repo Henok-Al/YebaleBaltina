0f7f2efb2f5818e29df6e36b6cc7f80b3f3729ad
Henok-Al <henok.gebresenbet@gmail.com>
2024-06-28 17:23:30 +0300

    create header and footer layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
 import {Button } from "@/components/ui/button"
-import { Search } from "lucide-react";
 export default function Home() {
   return (
     <div>
      <Button>Button</Button>
-     <Search>search</Search>
     </div>
   );
 }
