f417de2548092fe647b130e375d32ca9a397a947
Henok-Al <henok.gebresenbet@gmail.com>
2024-06-28 16:29:51 +0300

    setup nextjs with shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import {Button } from "@/components/ui/button"
+import { Search } from "lucide-react";
+export default function Home() {
+  return (
+    <div>
+     <Button>Button</Button>
+     <Search>search</Search>
+    </div>
+  );
+}
