fec9c06c255bf0aa2d9f95e0c986e4a74b2e64cd
Henok-Al <henok.gebresenbet@gmail.com>
2024-07-21 17:51:41 +0300

    payment method implemented

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,22 +3,28 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "An BeliBaltina built with Next.js, Postgres, Shadcn";
 
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(", ")
+  : ["PayPal", "Stripe", "Chapa", "CashOnDelivery"];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
+
 export const signInDefaultValues = {
   email: "",
   password: "",
 };
 
 export const signUpDefaultValues = {
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
-}
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export const shippingAddressDefaultValues = {
-  fullName: '',
-  streetAddress: '',
-  city: '',
-  postalCode: '',
-  country: '',
-}
+  fullName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "",
+};
