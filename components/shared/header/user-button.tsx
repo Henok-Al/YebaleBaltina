306455181b497d1f0a91c710b940f567109f6c53
Henok-Al <henok.gebresenbet@gmail.com>
2024-07-25 21:04:20 +0300

    implement order history and pagination

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -1,23 +1,23 @@
-import Link from 'next/link'
-import { auth } from '@/auth'
-import { Button } from '@/components/ui/button'
+import Link from "next/link";
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { SignOut } from '@/lib/actions/user.actions'
+} from "@/components/ui/dropdown-menu";
+import { SignOut } from "@/lib/actions/user.actions";
 
 export default async function UserButton() {
-  const session = await auth()
+  const session = await auth();
   if (!session)
     return (
       <Link href="/api/auth/signin">
         <Button>Sign In</Button>
       </Link>
-    )
+    );
   return (
     <div className="flex gap-2 items-center">
       <DropdownMenu>
@@ -44,6 +44,12 @@ export default async function UserButton() {
           </DropdownMenuLabel>
 
           <DropdownMenuItem className="p-0 mb-1">
+            <DropdownMenuItem>
+              <Link className="w-full" href="/user/orders">
+                Order History
+              </Link>
+            </DropdownMenuItem>
+
             <form action={SignOut} className="w-full">
               <Button
                 className="w-full py-4 px-2 h-4 justify-start"
@@ -56,5 +62,5 @@ export default async function UserButton() {
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
-  )
+  );
 }
